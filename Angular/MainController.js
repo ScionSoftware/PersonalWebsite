8b2c4d16c117232962f3c2d0d4bd716ec743723b
SpykeBytes <DeveloperSpike@gmail.com>
2013-12-24 14:11:32 -0500

    Fixed about link. Fixed reference to btn-primary. Added some blog samples. Styled the repeater a bit.

--- Angular/MainController.js
+++ Angular/MainController.js
@@ -4,12 +4,11 @@ var mainCtrl = function ($scope) {
 	var index = 0;
 	
 	var blogTest = function (name) {
-		return 
-		{ 
-			Order: index++, 
-			Title: name, 
-			Content: 'Lorem ipsum dolor...' 
-		}
+	    return {
+	        Order: index++,
+	        Title: name,
+	        Content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+	    };
 	};
 	
 	$scope.BlogPosts = [
