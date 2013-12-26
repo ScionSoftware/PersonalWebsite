6411a0f545a2c550b28b32b34da27b7e470eaaa3
SpykeBytes <DeveloperSpike@gmail.com>
2013-12-25 23:35:27 -0500

    Added current blogs. tested out the ng-include template.

--- Angular/MainController.js
+++ Angular/MainController.js
@@ -3,19 +3,18 @@
 var mainCtrl = function ($scope) {
 	var index = 0;
 	
-	var blogTest = function (name) {
+	var addTemplate = function (templateTitle) {
 	    return {
-	        Order: index++,
-	        Title: name,
-	        Content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+	        Title: templateTitle,
+	        Template: '/blogs/' + templateTitle + '.html'
 	    };
 	};
 	
 	$scope.BlogPosts = [
-		blogTest("Test1"),
-		blogTest("Winning"),
-		blogTest("Woot woot"),
-		blogTest("SOOO WEEEEE"),
+		addTemplate('hello-world'),
+		addTemplate("an-intro-to-redis"),
+		addTemplate("middle-eastern-scapegoat"),
+		addTemplate("sql-performance-tips"),
 	];
 	
 };
