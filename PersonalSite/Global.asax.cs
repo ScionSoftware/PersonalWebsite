5a98c0e59e054ca07717bc5df4c9136f5091cdb9
Colin Higgins <colin@spykebytes.me>
2015-06-24 05:48:50 -0400

    Migration to MVC3 for hosting provider. More mobile menu stuff.

---
+++ PersonalSite/Global.asax.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PersonalSite
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{action}/{name}", // URL with parameters
+                new { controller = "Home", action = "Index", name = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            // Use LocalDB for Entity Framework by default
+            Database.DefaultConnectionFactory = new SqlConnectionFactory(@"Data Source=(localdb)\v11.0; Integrated Security=True; MultipleActiveResultSets=True");
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
