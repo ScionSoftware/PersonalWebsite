b7422c17176e708083e90751d2bc68aa746e8c73
Colin Higgins <colin@spykebytes.me>
2015-07-16 10:42:06 -0400

    Banner ad at the bottom, because I am a jerk, and also redirects to one site

--- PersonalSite/Controllers/HomeController.cs
+++ PersonalSite/Controllers/HomeController.cs
@@ -5,137 +5,14 @@ using System.Web.Mvc;
 using System.Web.UI;
 using System.Xml.Linq;
 using PersonalSite.Models;
+using PersonalSite.CustomFilters;
 
 namespace PersonalSite.Controllers
 {
+    [MasterUrlRedirect]
     public class HomeController : Controller
     {
         private const int PreviewGroupAmount = 3;
-
-        [HttpGet]
-        public JsonResult PreviewsByIndex(int index)
-        {
-            var entries = GetOrderedBlogMetadata();
-
-            var toSkip = PreviewGroupAmount * index;
-
-            var previewsToRetrieve = entries.Skip(toSkip).Take(PreviewGroupAmount);
-
-            var previews =
-                previewsToRetrieve
-                .Select(i => new BlogViewModel()
-                {
-                    Name = i.Name,
-                    Published = i.Published,
-                    Content = GetBlogContent(i.Name)
-                });
-
-            return Json(previews, JsonRequestBehavior.AllowGet);
-        }
-
-        private string GetBlogContent(string name)
-        {
-            var blogDirectory = Server.MapPath("~/blogs");
-
-            var potentialEntry = blogDirectory + "\\" + name + ".html";
-
-            if (!System.IO.File.Exists(potentialEntry))
-                return null;
-
-            var content = System.IO.File.ReadAllText(potentialEntry);
-
-            return content;
-        }
-        private string GetBookContent(string name)
-        {
-            var blogDirectory = Server.MapPath("~/readings");
-
-            var potentialEntry = blogDirectory + "\\" + name + ".html";
-
-            if (!System.IO.File.Exists(potentialEntry))
-                return null;
-
-            var content = System.IO.File.ReadAllText(potentialEntry);
-
-            return content;
-        }
-
-        private List<BookViewModel> GetOrderedBooks()
-        {
-            var metadataDirectory = Server.MapPath("~/readings/metadata");
-
-            var metadataPath = metadataDirectory + "/book-metadata.xml";
-
-            var blogMetaData = XElement.Load(metadataPath);
-
-            var entries = blogMetaData.Elements("Entry").Select(n =>
-                new BookViewModel()
-                {
-                    Name = n.Attribute("Name").Value,
-                    Content = GetBookContent(n.Attribute("Name").Value)
-                }).ToList();
-            return entries;
-        }
-
-        [HttpGet]
-        public ActionResult Index()
-        {
-            var entries = GetOrderedBlogMetadata();
-
-            var previews = GetContentForBlogNames(entries, 3);
-
-            var model = new HomeViewModel()
-            {
-                Metadatas = entries,
-                Previews = previews.ToArray(),
-            };
-
-            return View(model);
-        }
-
-        private List<BlogViewModel> GetContentForBlogNames(IEnumerable<BlogMetadataViewModel> entries, int amount)
-        {
-            var previews = new List<BlogViewModel>();
-
-            foreach (var entry in entries)
-            {
-                amount--;
-
-                var content = GetBlogContent(entry.Name);
-
-                if (content != null)
-                    previews.Add(new BlogViewModel()
-                    {
-                        Content = content,
-                        Name = entry.Name,
-                        Published = entry.Published
-                    });
-
-                if (amount < 1)
-                    break;
-            }
-            return previews;
-        }
-
-        private BlogMetadataViewModel[] GetOrderedBlogMetadata()
-        {
-            var metadataDirectory = Server.MapPath("~/blogs/metadata");
-
-            var metadataPath = metadataDirectory + "/blog-metadata.xml";
-
-            var blogMetaData = XElement.Load(metadataPath);
-
-            var entries = blogMetaData.Elements("Entry").Select(n =>
-                new BlogMetadataViewModel()
-                {
-                    Name = n.Attribute("Name").Value,
-                    Published = DateTime.Parse(n.Attribute("Published").Value)
-                })
-                .Where(i => i.Published <= DateTime.Now)
-                .ToArray();
-
-            return entries;
-        }
 
         [HttpGet]
         public ActionResult Blog(string name)
@@ -183,5 +60,129 @@ namespace PersonalSite.Controllers
             return View(model);
         }
 
+        [HttpGet]
+        public JsonResult PreviewsByIndex(int index)
+        {
+            var entries = GetOrderedBlogMetadata();
+
+            var toSkip = PreviewGroupAmount * index;
+
+            var previewsToRetrieve = entries.Skip(toSkip).Take(PreviewGroupAmount);
+
+            var previews =
+                previewsToRetrieve
+                .Select(i => new BlogViewModel()
+                {
+                    Name = i.Name,
+                    Published = i.Published,
+                    Content = GetBlogContent(i.Name)
+                });
+
+            return Json(previews, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var entries = GetOrderedBlogMetadata();
+
+            var previews = GetContentForBlogNames(entries, 3);
+
+            var model = new HomeViewModel()
+            {
+                Metadatas = entries,
+                Previews = previews.ToArray(),
+            };
+
+            return View(model);
+        }
+
+        private string GetBlogContent(string name)
+        {
+            var blogDirectory = Server.MapPath("~/blogs");
+
+            var potentialEntry = blogDirectory + "\\" + name + ".html";
+
+            if (!System.IO.File.Exists(potentialEntry))
+                return null;
+
+            var content = System.IO.File.ReadAllText(potentialEntry);
+
+            return content;
+        }
+        private string GetBookContent(string name)
+        {
+            var blogDirectory = Server.MapPath("~/readings");
+
+            var potentialEntry = blogDirectory + "\\" + name + ".html";
+
+            if (!System.IO.File.Exists(potentialEntry))
+                return null;
+
+            var content = System.IO.File.ReadAllText(potentialEntry);
+
+            return content;
+        }
+
+        private List<BookViewModel> GetOrderedBooks()
+        {
+            var metadataDirectory = Server.MapPath("~/readings/metadata");
+
+            var metadataPath = metadataDirectory + "/book-metadata.xml";
+
+            var blogMetaData = XElement.Load(metadataPath);
+
+            var entries = blogMetaData.Elements("Entry").Select(n =>
+                new BookViewModel()
+                {
+                    Name = n.Attribute("Name").Value,
+                    Content = GetBookContent(n.Attribute("Name").Value)
+                }).ToList();
+            return entries;
+        }
+
+        private List<BlogViewModel> GetContentForBlogNames(IEnumerable<BlogMetadataViewModel> entries, int amount)
+        {
+            var previews = new List<BlogViewModel>();
+
+            foreach (var entry in entries)
+            {
+                amount--;
+
+                var content = GetBlogContent(entry.Name);
+
+                if (content != null)
+                    previews.Add(new BlogViewModel()
+                    {
+                        Content = content,
+                        Name = entry.Name,
+                        Published = entry.Published
+                    });
+
+                if (amount < 1)
+                    break;
+            }
+            return previews;
+        }
+
+        private BlogMetadataViewModel[] GetOrderedBlogMetadata()
+        {
+            var metadataDirectory = Server.MapPath("~/blogs/metadata");
+
+            var metadataPath = metadataDirectory + "/blog-metadata.xml";
+
+            var blogMetaData = XElement.Load(metadataPath);
+
+            var entries = blogMetaData.Elements("Entry").Select(n =>
+                new BlogMetadataViewModel()
+                {
+                    Name = n.Attribute("Name").Value,
+                    Published = DateTime.Parse(n.Attribute("Published").Value)
+                })
+                .Where(i => i.Published <= DateTime.Now)
+                .ToArray();
+
+            return entries;
+        }
     }
 }
---
+++ PersonalSite/CustomFilters/MasterUrlRedirect.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace PersonalSite.CustomFilters
+{
+    public class MasterUrlRedirect : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var requestUrl = filterContext.RequestContext.HttpContext.Request.Url;
+
+            var redirectPatterns = new[]
+            {
+                "spykebytes.me",
+                "spykebytes.com",
+                "www.higginsninja"
+            };
+
+            var shouldRedirect = redirectPatterns.Any(i => requestUrl.Host.Contains(i));
+
+            if (shouldRedirect)
+            {
+                var redirectUrl = "http://higginsninja.net" + requestUrl.PathAndQuery;
+
+                filterContext.Result = new RedirectResult(redirectUrl, permanent: true);
+            }
+        }
+    }
+}
