4d63012b8b6b16192799c00c421c660d7f6f7cce
Colin Higgins <colin.higgins@particular.net>
2016-02-22 21:31:50 -0500

    github stub loader

--- PersonalSite/DataAccess/GithubFileLoader.cs
+++ PersonalSite/DataAccess/GithubFileLoader.cs
@@ -1,42 +1,8 @@
 using System;
-using System.IO;
-using System.Web;
 using System.Xml.Linq;
 
 namespace PersonalSite.DataAccess
 {
-    public class LocalFileLoader : AbstractFileLoader
-    {
-        public override XElement LoadXmlContent(string relativePathWithoutExtension)
-        {
-            var xmlPath = FilePath($"~/{relativePathWithoutExtension}.xml");
-            return XElement.Load(xmlPath);
-        }
-
-        public override string LoadHtmlContent(string relativePathWithoutExtension)
-        {
-            var htmlPath = FilePath($"~/{relativePathWithoutExtension}.html");
-            var markdownPath = FilePath($"~/{relativePathWithoutExtension}.md");
-
-            if (File.Exists(htmlPath))
-            {
-                return ConvertContent(File.ReadAllText(htmlPath), ContentType.Html);
-            }
-
-            if (File.Exists(markdownPath))
-            {
-                return ConvertContent(File.ReadAllText(markdownPath), ContentType.Html);
-            }
-
-            return null;
-        }
-
-        protected string FilePath(string webPath)
-        {
-            return HttpContext.Current.Server.MapPath(webPath);
-        }
-    }
-
     public class GithubFileLoader : AbstractFileLoader
     {
         public override XElement LoadXmlContent(string relativePathWithoutExtension)
